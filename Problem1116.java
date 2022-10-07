013cc952b87e30ffd0f2ffbb16dc532f466768d6
guilherme vieira <guizinho.vfs@gmail.com>
2022-10-07 05:23:55 -0300

    Problema 1116

---
+++ Problem1116.java
@@ -0,0 +1,21 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int X, Y;
+        for (int i = 0; i < N; i++) {
+            X = leitor.nextInt();
+            Y = leitor.nextInt();
+            if (Y == 0)
+                System.out.println("divisao impossivel");
+            else
+                System.out.println((double) X / Y);
+        }
+    }
+
+}
